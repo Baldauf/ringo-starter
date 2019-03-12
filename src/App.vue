f9f92c70e252f5cc0f7ebcfb0a4aaf95ea6ccc82
Shane <shane.baldauf@gmail.com>
2019-03-12 13:20:50 -0600

    start starter

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/ringo-styleguide@1.0.6/dist/css/min/main.css">
+    <ASpot 
+      :blok="{
+        title: 'Knowledgebase', 
+        caption: 'This is an important message This is an important message This is an'
+      }"
+      :mods="['simple-color']" />
+      
+  </div>
+</template>
+
+<script>
+import Ringo from 'ringo-styleguide'
+
+export default {
+  name: 'app',
+  components: {
+    ...Ringo
+  },
+}
+</script>
